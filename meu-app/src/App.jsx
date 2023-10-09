3ed98a0861d21bcae8e69614e878a6edd3524f51
Lincoln Silva <106344749+Lincvasc@users.noreply.github.com>
2023-10-09 19:54:32 -0300

    Ex01_React

---
+++ meu-app/src/App.jsx
@@ -0,0 +1,31 @@
+/*Desenvolva e rode um programa react, evidenciando o passo a passo da instalação.*/
+
+/* 1. instalação do Node.js
+
+2. Instalar O Vite
+npm install -g create-vite
+
+3. Criar um novo projeto Vite
+create-vite nome-do-projeto --template react
+
+4. Navegar para o diretório do projeto
+cd nome-do-projeto
+
+5. Iniciar o aplicativo Vite
+npm install
+npm run dev
+
+*/
+
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Meu App React</h1>
+    </div>
+  )
+
+}
+
+export default App
